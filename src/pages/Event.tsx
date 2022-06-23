6716d32b08e52f4d07f0b0abf0631b5f670d1ea8
Joadson Silva <joadson83@hotmail.com>
2022-06-23 00:03:21 -0300

    built sidebar with lesson styles

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -5,11 +5,12 @@ import { Video } from "../components/Video";
 
 export function Event(){
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <Lesson />
-            <Sidebar />
-            <Video />
-        </>
+            <main className="flex flex-1">
+                <Video />
+                <Sidebar />
+            </main>
+        </div>
     );
 }
